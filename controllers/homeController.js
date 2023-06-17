40c54d3d03fcc2b95af5e048bf5e04b7dc4a6213
nina <ninagbs@abv.bg>
2023-06-17 18:17:23 +0300

    add home view

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,10 +1,14 @@
+const { getAll } = require('../services/hotelService');
+
 const homeController = require('express').Router();
 
 // TODO replace with real controller by assignment
-homeController.get('/', (req,res)=>{
+homeController.get('/', async (req,res)=>{
+    const hotels = await getAll().lean()
     res.render('home', {
         title: 'Home Page',
-        user: req.user
+        user: req.user,
+        hotels
     })
 })
 
